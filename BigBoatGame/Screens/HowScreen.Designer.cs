42f7d200b021d26aaeb20983962cc5529b6ba9cf
Andrew Henderson <andrhend062@ed.amdsb.ca>
2019-05-23 09:03:05 -0400

    g

--- BigBoatGame/Screens/HowScreen.Designer.cs
+++ BigBoatGame/Screens/HowScreen.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // menuButton
+            // 
+            this.menuButton.Location = new System.Drawing.Point(936, 500);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(75, 23);
+            this.menuButton.TabIndex = 0;
+            this.menuButton.Text = "button1";
+            this.menuButton.UseVisualStyleBackColor = true;
             // 
             // HowScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
+            this.Controls.Add(this.menuButton);
             this.Name = "HowScreen";
             this.Size = new System.Drawing.Size(1300, 750);
             this.ResumeLayout(false);
@@ -42,5 +53,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button menuButton;
     }
 }
