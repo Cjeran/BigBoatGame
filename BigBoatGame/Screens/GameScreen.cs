c151543c483c84efb6d30259727c1a480cee3dd8
garrsaun253 <garrsaun253@SCS-D-360QHY>
2019-06-10 09:12:41 -0400

    jjj

--- BigBoatGame/Screens/GameScreen.cs
+++ BigBoatGame/Screens/GameScreen.cs
@@ -172,7 +172,7 @@ namespace BigBoatGame.Screens
                         {
                             if (GameForm.vs)
                             {
-                                GameOver("you lose carrier ded", "MenuScreen");
+                                GameOver("", "MenuScreen");
                             }
                             else
                             {
@@ -373,7 +373,7 @@ namespace BigBoatGame.Screens
                     waves++;
                     if (waves == 6)
                     {
-                        GameOver("you lose carrier ded", "EndScreen");
+                        GameOver("Congratulations! You've fought off all the enemies!", "EndScreen");
                     }
                     if (GameForm.yank)
                     {
@@ -449,7 +449,8 @@ namespace BigBoatGame.Screens
                     if (carrier.hp <= 0)
                     {
                         carrier.hp = 0;
-                        GameOver("you lose carrier ded","EndScreen");
+                        GameOver("The carrier has been destroyed. This is a shameful display!", "EndScreen");
+                        break;
                     }
 
                 }
