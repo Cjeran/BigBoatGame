4a19649524bb5d11aa3f4c54e3ed16e01e0423f5
Cjeran <cierdieb649@ed.amdsb.ca>
2019-06-13 10:04:30 -0400

    HowScreen

--- BigBoatGame/Screens/HowScreen.Designer.cs
+++ BigBoatGame/Screens/HowScreen.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.components = new System.ComponentModel.Container();
             this.howToTimer = new System.Windows.Forms.Timer(this.components);
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.leftLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // howToTimer
@@ -37,21 +44,88 @@
             this.howToTimer.Interval = 16;
             this.howToTimer.Tick += new System.EventHandler(this.howToTimer_Tick);
             // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Mongolian Baiti", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(278, 25);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TitleLabel.Size = new System.Drawing.Size(744, 69);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Welcome To Flight School";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BigBoatGame.Properties.Resources.Joystick2;
+            this.pictureBox1.Location = new System.Drawing.Point(125, 500);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BigBoatGame.Properties.Resources.Joystick;
+            this.pictureBox2.Location = new System.Drawing.Point(125, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 603);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hold Joystick \'Right\' To Turn Right";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // leftLabel
+            // 
+            this.leftLabel.BackColor = System.Drawing.Color.Transparent;
+            this.leftLabel.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftLabel.Location = new System.Drawing.Point(97, 303);
+            this.leftLabel.Name = "leftLabel";
+            this.leftLabel.Size = new System.Drawing.Size(163, 35);
+            this.leftLabel.TabIndex = 4;
+            this.leftLabel.Text = "Hold Joystick \'Left\' To Turn Left";
+            this.leftLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // HowScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 11F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Coral;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.Controls.Add(this.leftLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TitleLabel);
+            this.Font = new System.Drawing.Font("Mongolian Baiti", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "HowScreen";
-            this.Size = new System.Drawing.Size(1300, 730);
+            this.Size = new System.Drawing.Size(1300, 750);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.HowScreen_Paint);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.HowScreen_KeyUp);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.HowScreen_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Timer howToTimer;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label leftLabel;
     }
 }
