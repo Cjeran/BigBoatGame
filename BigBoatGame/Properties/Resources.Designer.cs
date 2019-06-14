61a05a4c0cde04ca1b94fdc2b72a91e5f38af01b
Cieran Diebolt <cierdieb649@ed.amdsb.ca>
2019-06-14 01:12:37 -0400

    Sounds

--- BigBoatGame/Properties/Resources.Designer.cs
+++ BigBoatGame/Properties/Resources.Designer.cs
@@ -251,6 +251,42 @@ namespace BigBoatGame.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Engine1 {
+            get {
+                return ResourceManager.GetStream("Engine1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Engine2 {
+            get {
+                return ResourceManager.GetStream("Engine2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Engine3 {
+            get {
+                return ResourceManager.GetStream("Engine3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Explosion {
+            get {
+                return ResourceManager.GetStream("Explosion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap F4F_4_Down {
@@ -378,6 +414,24 @@ namespace BigBoatGame.Properties {
             get {
                 object obj = ResourceManager.GetObject("Lexington", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream MachineGun {
+            get {
+                return ResourceManager.GetStream("MachineGun", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Secondary {
+            get {
+                return ResourceManager.GetStream("Secondary", resourceCulture);
             }
         }
         
